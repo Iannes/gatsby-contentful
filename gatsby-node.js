755d9c75fbdd94dfb39325cebb98934ae45029da
@yannis_spyrou <yannis.spyrou@vistaprint.com>
2020-10-17 14:26:19 +0200

    add product template logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,13 @@
-const Promise = require('bluebird')
-const path = require('path')
+const Promise = require("bluebird");
+const path = require("path");
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
+    const blogPost = path.resolve("./src/templates/blog-post.js");
+    const productTemplate = path.resolve("./src/templates/product.js");
+
     resolve(
       graphql(
         `
@@ -20,23 +22,34 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         `
-      ).then(result => {
+      ).then((result) => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
 
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach(post => {
+        const posts = result.data.allContentfulBlogPost.edges;
+        posts.forEach((post) => {
           createPage({
             path: `/blog/${post.node.slug}/`,
             component: blogPost,
             context: {
               slug: post.node.slug,
             },
-          })
-        })
+          });
+        });
+
+        // const products = result.data.allContentfulBlogPost.edges;
+        // products.forEach((post) => {
+        //   createPage({
+        //     path: `/products/${post.node.slug}/`,
+        //     component: productTemplate,
+        //     context: {
+        //       slug: product.node.slug,
+        //     },
+        //   });
+        // });
       })
-    )
-  })
-}
+    );
+  });
+};
